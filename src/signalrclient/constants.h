c07a2d85df4a2d390d5dd6ccdf4da177dcd176d4
moozzyk <pawelka@microsoft.com>
2015-02-13 11:57:27 -0800

    0.0.0 - support versioning in build

--- src/signalrclient/constants.h
+++ src/signalrclient/constants.h
@@ -5,6 +5,6 @@
 
 #include "cpprest\details\basic_types.h"
 
-#define SIGNALR_VERSION _XPLATSTR("3.0.0")
+#define SIGNALR_VERSION _XPLATSTR("1.0.0-alpha")
 #define PROTOCOL _XPLATSTR("1.4")
 #define USER_AGENT _XPLATSTR("SignalR.Client.Cpp/")SIGNALR_VERSION
